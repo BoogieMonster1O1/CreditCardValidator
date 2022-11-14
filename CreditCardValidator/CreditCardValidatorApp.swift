386ac7c43647176dabf042c734b8dd0349dc240a
Shrish Deshpande <harrypotter.starwars.shrish@gmail.com>
2022-11-14 08:48:41 +0530

    Initial Commit

---
+++ CreditCardValidator/CreditCardValidatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreditCardValidatorApp.swift
+//  CreditCardValidator
+//
+//  Created by Shrish Deshpande on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct CreditCardValidatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
