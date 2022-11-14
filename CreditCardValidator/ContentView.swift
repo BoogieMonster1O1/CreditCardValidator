d07fb62754cf4703e85c1cfadf0b3f20ad0b8cb6
Shrish Deshpande <harrypotter.starwars.shrish@gmail.com>
2022-11-14 09:32:19 +0530

    Frontend

--- CreditCardValidator/ContentView.swift
+++ CreditCardValidator/ContentView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var text: String = ""
+    @State private var valid: Bool? = nil
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Hello, world!")
+                .font(.largeTitle)
+                .padding(.all, 3.0)
+                .accessibilityLabel("Credit Card Validator")
+            HStack {
+                Spacer()
+                TextField("Credit Card Number", text: $text)
+                    .padding(.all)
+                    .accessibilityLabel("Credit Card Number")
+                Button(action: {
+                    
+                }, label: {
+                    Text("Verify")
+                        .padding(.all)
+                })
+                    .accessibilityLabel("Validate Card Number")
+                Spacer()
+            }
+            Spacer()
+            if let valid = valid {
+                if (valid) {
+                    Text("Valid")
+                } else {
+                    Text("Invalid")
+                }
+            }
+            Spacer()
+        }
     }
 }
 
